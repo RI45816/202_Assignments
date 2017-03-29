1859f69ee64133fbe3c9d58b311e6410ad1d8dfe
RI45816 <uu3@umbc.edu>
2017-03-28 20:47:35 -0400

    @U733O@

--- Projects/proj3/nbproject/Makefile-Debug.mk
+++ Projects/proj3/nbproject/Makefile-Debug.mk
@@ -34,7 +34,8 @@ include Makefile
 OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES= \
+	${OBJECTDIR}/Sequencer.o
 
 
 # C Compiler Flags
@@ -59,7 +60,12 @@ LDLIBSOPTIONS=
 
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/proj3.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/proj3 ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/proj3 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Sequencer.o: Sequencer.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Sequencer.o Sequencer.cpp
 
 # Subprojects
 .build-subprojects:
