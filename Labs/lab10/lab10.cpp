d0026192fc31347e0fd29f6ddbcc00de31a81db9
RI45816 <uu3@umbc.edu>
2017-04-26 20:34:05 -0400

    064J0

--- Labs/lab10/lab10.cpp
+++ Labs/lab10/lab10.cpp
@@ -12,7 +12,25 @@ int main()
   
   ia+5;
   
-  // Print out the number of errors/edge cases found
+  ia[4];
+  ia.Insert(5,5);
+  ia.Clear();
+  ia.Clear();
+  ia.Remove(5);
+  ia[44];
+  ia+5;
+  ia.Del(5);
+  ia.Contains(7);
+  ia.GetLength();
+  ia.AddToAll(43);
+  ia.Scale(235);
+  ia.Sum(IntArray(234));
+  ia.Product(IntArray(43));
+  ia.Insert(12,3);
+  ia[34];
+  ia == IntArray(53);
+
+// Print out the number of errors/edge cases found
   cout << endl << endl << IntArray::GetProgress() << endl;
     
   return 0;
