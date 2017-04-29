1b876a58491e08329a113f64ba2b7227dc61f30f
RI45816 <uu3@umbc.edu>
2017-04-28 20:21:24 -0400

    @95HRI@

--- Labs/lab11/lab11.cpp
+++ Labs/lab11/lab11.cpp
@@ -6,67 +6,83 @@ const int START = 0;
 const int END = 10;
 
 struct Node {
-  int num;
-  Node * next;
+    int num;
+    Node * next;
+
+    Node(int num) : num(num) {
+        next = NULL;
+    };
 };
 
-class LinkedList{
+class LinkedList {
 public:
-  LinkedList(){m_head = NULL;}
-  ~LinkedList(){
-    Node * ptrNode = m_head;    
-    while(ptrNode != NULL){
-      ptrNode = m_head->next;
-      delete m_head;
-      m_head = ptrNode;
-    }
-  }
-  void insert(int value, bool location){
-    //If the linked list is empty
-    if (m_head == NULL){
-      //Step 1: Code when linked list is empty
 
+    LinkedList() {
+        m_head = NULL;
     }
 
-    //If we are inserting in the beginning of the linked list
-    else if(location == false){
-      //Step 2: Code when you are doing an insert beginning
+    ~LinkedList() {
+        Node * ptrNode = m_head;
+        while (ptrNode != NULL) {
+            ptrNode = m_head->next;
+            delete m_head;
+            m_head = ptrNode;
+        }
+    }
+
+    void insert(int value, bool location) {
+        //If the linked list is empty
+
+        Node * node;
+        node = new Node(value);
+        if (m_head == NULL) {
+            m_head = node;
+
+        }//If we are inserting in the beginning of the linked list
+        else if (location == false) {
+            node->next = m_head;
+            m_head = node;
+
+
+        }//If we are inserting in the end of the linked list
+            //**RECURSIVE**
+            //Calls insertHelper
+        else if (location == true) {
+            insertHelper(m_head, value);
+        }
+    }
+
+    void insertHelper(Node *curNode, int value) {
+        if (curNode->next)
+            insertHelper(curNode->next, value);
+        else
+            curNode->next = new Node(value);
 
 
     }
-    //If we are inserting in the end of the linked list
-    //**RECURSIVE**
-    //Calls insertHelper
-    else if(location == true){
-      //Step 3: Code when you are doing an insert end (recursively)
 
+    //Iterates through linked list and displays
+
+    void display() {
+        Node * ptrNode = m_head;
+        while (ptrNode) {
+            cout << ptrNode->num << "-->";
+            ptrNode = ptrNode->next;
+        }
+        cout << "END" << endl;
     }
-  }
-  void insertHelper(Node *curNode, int value){
-    //Step 4: Code the recursive function for insert end
-
- }
-
-  //Iterates through linked list and displays
-  void display(){
-    Node * ptrNode = m_head;
-    while(ptrNode){
-      cout << ptrNode->num << "-->";
-      ptrNode = ptrNode->next;
-    }
-    cout << "END" << endl;
-  }
 
 private:
-  Node* m_head;
+    Node* m_head;
 };
 
 //Creates linked list and inserts 0-9 then displays
-int main (){
-  LinkedList ll;
-  for(int i = START; i < END;i++){
-    ll.insert(i, 1);
-  }
-  ll.display();
-  return 0;
+
+int main() {
+    LinkedList ll;
+    for (int i = START; i < END; i++) {
+        ll.insert(i, 1);
+    }
+    ll.display();
+    return 0;
 }
